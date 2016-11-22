11ec9756079d199331f1dc9348985e7ad7ce4117
BuildTools <grucemax@uw.edu>
2016-11-21 20:22:00 -0800

    Added methods to maxtrix class, getRow, scalarMultiply, and isSafeRow

--- Matrix.java
+++ Matrix.java
@@ -17,4 +17,41 @@ public class Matrix {
         }
     }
 
+    /**
+     * Multiplies a rowNum of the matrix by the inputted scalar.
+     * @param rowNum The rowNum number that is to be scaled.
+     * @param scalar The number the rowNum elements will be sacled by.
+     */
+    public void scalarMultiplyRow(int rowNum, double scalar) {
+        if (this.isSafeRowNum) {
+            for (int i = 0; i < this.getColumnSize(); i++) {
+                matrix[rowNum][i] *= scalar;
+            }
+        }
+        //TODO add exception
+    }
+
+    /**
+     * Returns a requested row.
+     * @param rowNum The row that is wanted.
+     * @return int array representing the row.
+     */
+    public int[] getRow(int rowNum) {
+        if (isSafeRowNum(rowNum)) {
+            int[] row = new int[this.getRowSize()];
+            for (int i = 0; i < this.getColumnSize(); i++) {
+                row[i] = matrix[rowNum][i];
+            }
+            return row;
+        }
+        //TODO add exception
+    }
+
+    /**
+     * Returns if the row exists.
+     * @param row The row number to be checked.
+     */
+    public boolean isSafeRowNum(int rowNum) {
+        return row >= 0 && row <= this.getRowSize();
+    }
 }
